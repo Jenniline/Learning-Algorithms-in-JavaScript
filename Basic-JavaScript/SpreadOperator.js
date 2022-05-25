b21023153e69cce5f306e8f3dbd012a75a55d54b
Jenniline <jennilineebai@gmail.com>
2022-05-25 16:40:18 +0100

    Learned Spread Operator and Rest Parameter in JavaScript. Highlight of lesson both have thesame syntax but rest parameter must be the last arguments of the function. However the Spread Operator can be anywhere

---
+++ Basic-JavaScript/SpreadOperator.js
@@ -0,0 +1,36 @@
+/** Use the Spread Operator to Evaluate Arrays In-Place */
+
+const arr = [20,30,40,50]
+const maximus = Math.max.apply(null,arr)
+
+/** We had to use Math.max.apply(null, arr) 
+ * because Math.max(arr) returns NaN. Math.max() 
+ * expects comma-separated arguments, but not an array. 
+ * The spread operator makes this syntax much better to read and maintain. */
+
+const maximus1 = Math.max(...arr)
+
+const arr1 = ['JAN','FEB','MARCH','APR','MAY']
+let arr2
+
+arr2 = [...arr1]
+console.log(arr2);
+
+/** Destructuring allows you to assign a new variable 
+ * name when extracting values. You can do this by putting
+ *  the new name after a colon when assigning the value. */
+
+/** Using the same object from the last example:*/ 
+ const user = { name: 'John Doe', age: 34 };
+/** Here's how you can give new variable names in the assignment:*/ 
+ const { name: userName, age: userAge } = user;
+
+ const source = [1,2,3,4,5,6]
+function removeFirstTwo(list) {
+    const [a,b, ...arrr] = list
+
+    return arrr;
+
+}
+
+ 
